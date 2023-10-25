3450944b2bbb4b0bf52fdc5fdfc49ed168a2a065
adityasachan24@gmail.com <adityasachan24@gmail.com>
2023-10-25 13:26:52 +0530

    check2

--- Gitbit/gitbit/lib/model.dart/page.dart
+++ Gitbit/gitbit/lib/model.dart/page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart'; // Import the lottie package
+import 'package:gitbit/screens/navigation.dart';
 
 class MyColors {
   static const Color darkGrey = Color(0xFF0F0F0F);
@@ -13,168 +13,152 @@ class MyUserDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MyColors.darkGrey,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "USER DETAILS",
-            style: TextStyle(fontSize: 28, color: Colors.white),
-          ),
-          SizedBox(height: 20),
-          UserDetailsForm(),
-        ],
-      ),
-    );
-  }
-}
-
-class UserDetailsForm extends StatefulWidget {
-  @override
-  _UserDetailsFormState createState() => _UserDetailsFormState();
-}
-
-class _UserDetailsFormState extends State<UserDetailsForm> {
-  final TextEditingController usernameController = TextEditingController();
-  final TextEditingController githubUsernameController = TextEditingController();
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-  final TextEditingController ageController = TextEditingController();
-  final TextEditingController bioController = TextEditingController();
-  bool receiveNotifications = false;
-  List<String> interests = [];
-
-  void toggleInterest(String interest) {
-    setState(() {
-      if (interests.contains(interest)) {
-        interests.remove(interest);
-      } else {
-        interests.add(interest);
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView( // Wrap the form in a SingleChildScrollView
-      child: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedTextField(usernameController, 'Username', Icons.person),
-            ElevatedTextField(githubUsernameController, 'GitHub Username', Icons.code),
-            ElevatedTextField(emailController, 'Email ID', Icons.email),
-            ElevatedTextField(passwordController, 'Password', Icons.lock, isPassword: true),
-            ElevatedTextField(ageController, 'Age', Icons.cake),
-            ElevatedTextField(bioController, 'Bio', Icons.info, maxLines: 3),
-            Row(
-              children: [
-                Text('Receive Notifications', style: TextStyle(color: Colors.white)),
-                Switch(
-                  value: receiveNotifications,
-                  onChanged: (value) {
-                    setState(() {
-                      receiveNotifications = value;
-                    });
-                  },
-                  activeColor: MyColors.tealGreen,
-                ),
-              ],
-            ),
-            SizedBox(height: 16.0),
-            Text('Select Your Interests:', style: TextStyle(color: Colors.white, fontSize: 16)),
-            InterestChips(interests, toggleInterest),
-            ElevatedTextField(usernameController, 'Additional Field 1', Icons.person),
-            ElevatedTextField(usernameController, 'Additional Field 2', Icons.person),
-            ElevatedTextField(usernameController, 'Additional Field 3', Icons.person),
-            SizedBox(height: 16.0),
-            LottieButton(), // Add the LottieButton
-          ],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: MyUserDetailsForm(),
         ),
       ),
     );
   }
 }
 
-class ElevatedTextField extends StatelessWidget {
-  final TextEditingController controller;
-  final String hintText;
-  final IconData icon;
-  final bool isPassword;
-  final int maxLines;
+class MyUserDetailsForm extends StatefulWidget {
+  @override
+  _MyUserDetailsFormState createState() => _MyUserDetailsFormState();
+}
 
-  ElevatedTextField(this.controller, this.hintText, this.icon,
-      {this.isPassword = false, this.maxLines = 1});
+class _MyUserDetailsFormState extends State<MyUserDetailsForm> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController githubIdController = TextEditingController();
+  final TextEditingController interestsController = TextEditingController();
+  final TextEditingController languagesController = TextEditingController();
+  final TextEditingController ageController = TextEditingController();
+  final TextEditingController linkedinController = TextEditingController();
+
+  // List of available interest options
+  final List<String> availableInterests = [
+    'Technology',
+    'Sports',
+    'Art',
+    'Music',
+    'Cooking',
+    'Travel',
+    'Photography',
+    'Gaming',
+    'Fashion',
+    'Books',
+    'Fitness',
+    'Movies',
+    'Science',
+    'Nature',
+    'Food',
+  ];
+
+  // List of available programming languages
+  final List<String> availableLanguages = [
+    'JavaScript',
+    'Python',
+    'Java',
+    'C++',
+    'Swift',
+    'Ruby',
+    'Go',
+    'Kotlin',
+    'Dart',
+    'PHP',
+    'C#',
+    'Rust',
+  ];
 
   @override
   Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 4.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                "User Details",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              buildTextField(nameController, 'Name', Icons.person),
+              buildTextField(emailController, 'Email', Icons.email),
+              buildTextField(githubIdController, 'GitHub ID', Icons.code),
+              buildTextField(interestsController, 'Interests', Icons.star),
+              buildInterestOptions(availableInterests, interestsController),
+              buildTextField(languagesController, 'Languages', Icons.language),
+              buildInterestOptions(availableLanguages, languagesController),
+              buildTextField(ageController, 'Age', Icons.cake),
+              buildTextField(linkedinController, 'LinkedIn', Icons.link),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                Navigator.pushReplacement(context, MaterialPageRoute(builder: (_)=>Homescreen()));
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(MyColors.navyBlue),
+                ),
+                child: Text('Save', style: TextStyle(color: Colors.white)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildTextField(
+      TextEditingController controller, String hintText, IconData icon) {
     return Container(
       margin: EdgeInsets.only(bottom: 16.0),
       child: TextField(
         controller: controller,
-        obscureText: isPassword,
-        maxLines: maxLines,
-        style: TextStyle(color: Colors.white),
+        style: TextStyle(color: Colors.black),
         decoration: InputDecoration(
           hintText: hintText,
-          hintStyle: TextStyle(color: MyColors.darkCyan),
+          prefixIcon: Icon(icon, color: MyColors.tealGreen),
           filled: true,
-          fillColor: MyColors.navyBlue.withOpacity(0.5),
+          fillColor: Colors.white,
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(8.0),
             borderSide: BorderSide.none,
-          ),
-          prefixIcon: Icon(
-            icon,
-            color: MyColors.tealGreen,
           ),
         ),
       ),
     );
   }
-}
 
-class InterestChips extends StatelessWidget {
-  final List<String> interests;
-  final Function(String) onInterestToggled;
-
-  InterestChips(this.interests, this.onInterestToggled);
-
-  @override
-  Widget build(BuildContext context) {
-    return Wrap(
-      spacing: 8.0,
-      children: interests.map((interest) {
-        return InputChip(
-          label: Text(interest),
-          onSelected: (isSelected) {
-            onInterestToggled(interest);
-          },
-          selected: interests.contains(interest),
-          backgroundColor: MyColors.tealGreen,
-          selectedColor: MyColors.navyBlue,
-          labelStyle: TextStyle(color: Colors.white),
-        );
-      }).toList(),
+  Widget buildInterestOptions(List<String> options, TextEditingController controller) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Wrap(
+        spacing: 8.0,
+        children: options.map((option) {
+          return GestureDetector(
+            onTap: () {
+              controller.text = option;
+            },
+            child: Chip(
+              label: Text(option),
+            ),
+          );
+        }).toList(),
+      ),
     );
   }
 }
 
-class LottieButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        // Handle button click action and navigation
-      },
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(MyColors.navyBlue),
-      ),
-      child: Lottie.asset(
-        'assets/WkjuwJnoFw.json', // Replace with your Lottie animation file
-        width: 100,
-        height: 100,
-      ),
-    );
-  }
+void main() {
+  runApp(MaterialApp(
+    home: MyUserDetailsPage(),
+  ));
 }
