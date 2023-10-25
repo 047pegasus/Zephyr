dc6226b40bd3645cc5e857b682e6124d14b3ec49
Vibhor <vibgitcode27@gmail.com>
2023-10-25 10:25:26 +0530

    .

--- MLH_WorkFlow/src/store/atoms/LogInPop.js
+++
@@ -1,8 +0,0 @@
-import { atom } from "recoil";
-
-export const LogInPop = atom({
-    key : "LogInPop" ,
-    default : {
-        Lpopup : false
-    } ,
-})
--- MLH_WorkFlow/src/store/atoms/signUpPop.js
+++
@@ -1,8 +0,0 @@
-import { atom } from "recoil";
-
-export const lodge = atom({
-    key : "lodge" ,
-    default : {
-        Spopup : false
-    } ,
-})
